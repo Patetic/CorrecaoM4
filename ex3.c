b8ee0724233760a4e27ff9d1b18bfba8bc251b67
YES123 <72169958+Patetic@users.noreply.github.com>
2023-03-16 14:04:31 +0000

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -16,9 +16,7 @@ main ()
   printf ("O primeiro nome em maiusculas:%s", nomes[0]);
   for (i = 0; i < COL; i++)
     nomes[1][i] = tolower(nomes[1][i]);
-  printf ("\nO tamanho do primeiro nome e:%d", strlen (nomes[0]));
-  printf ("\nO tamanho do segundo nome e:%d", strlen (nomes[1]));
+  printf ("\nO tamanho do primeiro nome e: %d", strlen (nomes[0])-1);
+  printf ("\nO tamanho do segundo nome e: %d", strlen (nomes[1])-1);
   return 0;
 }
-
-//incompleto
